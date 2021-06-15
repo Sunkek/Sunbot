d7e06485781b010fb0ddd91183f79bec56e02208
Sunkek <xexc@mail.ru>
2021-06-15 14:15:12 +0500

    Convert header to str

--- discord-bot/utils/reaction_roles.py
+++ discord-bot/utils/reaction_roles.py
@@ -14,7 +14,7 @@ async def new_reaction_roles(bot, user_id, guild_id, channel_id, message_id):
     """Create new reaction roles message"""
     return await bot.web_client.post(
         f"{URLS['reaction_roles_message']}", 
-        headers={"Invoker": user_id},
+        headers={"Invoker": str(user_id)},
         json={
             "guild_id": guild_id, 
             "channel_id": channel_id, 
