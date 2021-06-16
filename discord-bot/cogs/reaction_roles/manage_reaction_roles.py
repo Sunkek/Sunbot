0084368d93c16aafc18cd9c19ab0286b0e3a67f4
Sunkek <xexc@mail.ru>
2021-06-16 10:55:09 +0500

    Make new_rr global

--- discord-bot/cogs/reaction_roles/manage_reaction_roles.py
+++ discord-bot/cogs/reaction_roles/manage_reaction_roles.py
@@ -10,16 +10,20 @@ from discord.ext import commands
 from utils import output, reaction_roles, parsers
 
 re_url_pattern = "([0-9]+)\/([0-9]+)\/([0-9]+)"
+new_rr = discord.Embed(
+    title="Reaction roles",
+    description="Click on the reactions below to get the roles",
+)
 
 async def add_reaction_role(bot, ctx, input_message, rr_message):
     emote, role = await parsers.parse_reaction_role_pair(input_message.content, ctx)
     res = await reaction_roles.add_reaction_role(
-        self.bot, ctx.author.id, emote, role.id, rr_message.id,
+        bot, ctx.author.id, emote, role.id, rr_message.id,
     )
     print(res.__dict__) # TODO work with res
 
     if rr_message.author == ctx.guild.me:
-        new_embed = rr_message.embeds[0] if rr_message.embeds else self.new_rr
+        new_embed = rr_message.embeds[0] if rr_message.embeds else new_rr
         roles_field = 0
         for num, field in enumerate(new_embed.fields):
             if field.name.lower() == "roles":
@@ -35,10 +39,6 @@ async def add_reaction_role(bot, ctx, input_message, rr_message):
 class ReactionRoles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.new_rr = discord.Embed(
-            title="Reaction roles",
-            description="Click on the reactions below to get the roles",
-        )
 
     @commands.group(
         name="reactionroles", 
@@ -56,7 +56,7 @@ class ReactionRoles(commands.Cog):
     )
     async def new_reaction_roles(self, ctx, message_url: typing.Union[discord.TextChannel, str]):
         if isinstance(message_url, discord.TextChannel):
-            message = await message_url.send(embed=self.new_rr)
+            message = await message_url.send(embed=new_rr)
             res = await reaction_roles.new_reaction_roles(
                 self.bot, ctx.author.id, ctx.guild.id, message_url.id, message.id
             )
@@ -69,7 +69,7 @@ class ReactionRoles(commands.Cog):
             message = await channel.fetch_message(int(m.group(3)))
             if message.author == ctx.guild.me:
                 if not message.embeds:
-                    await message.edit(embed=self.new_rr)       
+                    await message.edit(embed=new_rr)       
             res = await reaction_roles.new_reaction_roles(
                 self.bot, ctx.author.id, guild.id, channel.id, message.id
             )
